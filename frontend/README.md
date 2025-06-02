2ba964330e874783acfee3b58db17d9ccb9196cf
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2025-06-03 00:02:32 +0300

    frontend & tailwind setup

---
+++ frontend/README.md
@@ -0,0 +1,4 @@
+
+## Requirments
+1. npm install tailwindcss @tailwindcss/vite
+2. npx tailwindcss init -p
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-2xl">
+      Sports Car Shop 🚗 - Ready to Build!
+    </div>
+  );
+}
+
+export default App;
+
